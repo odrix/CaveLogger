495c3ec46e8b6e39b7c7b1e1532a5cc57aa867dd
adriw <adribey@free.Fr>
2013-10-06 23:29:19 +0200

    tentative de sleep pour économiser la conso

--- Arduino_prog/EnvLogger/EnvLogger.ino
+++ Arduino_prog/EnvLogger/EnvLogger.ino
@@ -1,8 +1,17 @@
-
 #include <dht11.h>
 #include <DS1302.h>
 #include <SD.h>
 #include <EEPROM.h>
+
+//Optimisation de la consommation
+#include <avr/power.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
+#include <avr/wdt.h>
+volatile int f_wdt=1;
+
+int nbWdt =0;
+const int NB_WDT_FOR_MINUTE = 7;
 
 // PIN et objet pour le capteur de temperature et humidité (DHT11)
 #define DHT11PIN 2
@@ -210,19 +219,83 @@ void setup()
     // don't do anything more:
     return;
   }
-  
+    
   init_ligth();
   init_time();
+    
+  power_adc_disable();  // Convertisseur Analog / Digital pour les entrées analogiques
+  power_timer1_disable();
+  power_timer2_disable();
+  setup_watchdog(9);
 }
 
 
 /*------------------------------ LOOP ----------------------------- */
 void loop()
-{
-  read_time();
-  read_temperature_humidity();
-  read_light();
-  log_data_line();
+{ 
+  if (f_wdt == 1) {
+    // Effectuer les mesures ici    
+    read_time();
+    read_temperature_humidity();
+    read_light();
+    log_data_line();
+
+    f_wdt = 0; // Ne pas oublier d'initialiser le flag
+    enterLongSleep(); //Revenir en mode veille
+  } else {
+    /* Do nothing. */
+  }
+}
+
+
+/*----------------------------- Economie d'énérgie -----------------*/
+// Watchdog Interrupt Service est exécité lors d'un timeout du WDT
+ISR(WDT_vect) {
+  if(f_wdt == 0) {
+    f_wdt = 1; // flag global 
+  }
+}
+// paramètre : 0=16ms, 1=32ms, 2=64ms, 3=128ms, 4=250ms, 5=500ms, 6=1 sec,7=2 sec, 8=4 sec, 9=8 sec
+void setup_watchdog(int ii) {
+  byte bb;
+  int ww;
+  if (ii > 9 ) ii=9;
+  bb=ii & 7;
+  if (ii > 7) bb|= (1<<5);
+  bb|= (1<<WDCE);
+  ww=bb;
+  // Clear the reset flag
+  MCUSR &= ~(1<<WDRF);
+  // start timed sequence
+  WDTCSR |= (1<<WDCE) | (1<<WDE);
+  // set new watchdog timeout value
+  WDTCSR = bb;
+  WDTCSR |= _BV(WDIE);
+}
+
+void enterLongSleep() {
+  //Optimisation de la consommation
+  power_spi_disable();
+  power_twi_disable();
+  // Si pas besoin de communiquer par l'usb
+  power_usart0_disable();
+  //Extinction des timers, attention timer0 utilisé par millis ou delay
+  power_timer0_disable();
   
-  delay(100);
+  for(nbWdt=0;nbWdt<2;nbWdt++)  //NB_WDT_FOR_MINUTE
+  {
+    enterSleep();
+  }
+  
+  /* Re-enable the peripherals. */
+  power_all_enable();
+}
+
+void enterSleep(void) {
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_enable();
+  sleep_mode(); //Entre dans le mode veille choisi
+
+  // Le programme va reprendre ici après le timeout du WDT
+  sleep_disable(); // La 1ère chose à faire est de désactiver le mode veille
 }
