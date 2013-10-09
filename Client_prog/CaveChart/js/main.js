ad9887bd13f3a6649f038933b2b495a3fc04f061
adriw <adribey@free.Fr>
2013-10-10 00:09:12 +0200

    affiche de l'hygrometrie

--- Client_prog/CaveChart/js/main.js
+++ Client_prog/CaveChart/js/main.js
@@ -1,5 +1,5 @@
 ﻿var margin = { top: 20, right: 20, bottom: 30, left: 50 },
-    width = 800 - margin.left - margin.right,
+    width = 1000 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 var parseDate = d3.time.format("%Y-%m-%d %H:%M:%S").parse;
@@ -10,10 +10,19 @@ var y = d3.scale.linear().range([height, 0]);
 var xAxis = d3.svg.axis().scale(x).orient("bottom");
 var yAxis = d3.svg.axis().scale(y).orient("left");
 
-var line = d3.svg.line()
+var lineTemperature = d3.svg.line()
     .x(function (d) { return x(d.d); })
-    .y(function (d) { return y(d.t); })
-    .interpolate('basis');
+    .y(function (d) { return y(d.t); });
+
+var lineHygrometrie = d3.svg.line()
+    .x(function (d) { return x(d.d); })
+    .y(function (d) { return y(d.h); });
+
+
+var area = d3.svg.area()
+    .x(function (d) { return x(d.d); })
+    .y0(height)
+    .y1(function (d) { return y(d.t); });
 
 var svg = d3.select("body").append("svg")
     .attr("width", width )
@@ -35,10 +44,7 @@ d3.json("/Data/CAVEDATA.ENV", function (error, json) {
 function trace(data) {
 
     x.domain(d3.extent(data, function (d) { return d.d; }));
-    var o = d3.extent(data, function (d) { return d.t; });
-    o[0] = parseInt(o[0]) - 10;
-    o[1] = parseInt(o[1]) + 10;
-    y.domain(o);
+    y.domain([d3.min(data, function (d) {return d.t;}) - 5, d3.max(data, function (d) {return d.h;})  - -5]); // l'operateur moins se fais que sur des number alors que le plus s'applique aussi sur des string
 
     svg.append("g")
         .attr("class", "x axis")
@@ -57,6 +63,11 @@ function trace(data) {
 
     svg.append("path")
         .datum(data)
-        .attr("class", "line")
-        .attr("d", line);
+        .attr("class", "temperature")
+        .attr("d", lineTemperature);
+
+    svg.append("path")
+        .datum(data)
+        .attr("class", "hygrometrie")
+        .attr("d", lineHygrometrie);
 }
